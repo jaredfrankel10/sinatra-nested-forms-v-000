e59b722eb5bb79b364747c652ee65cc9db6a2c39
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 20:48:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,21 +1,28 @@
 require './environment'
-require 'pry'
-module FormsLab
-  class App < Sinatra::Base
-    set :views, Proc.new { File.join(root, "views/pirates") }
-     
-         get '/' do
-           erb :root
-         end
-         get '/new' do
-           erb :new
-         end
-               post '/pirates' do
-           #binding.pry
-           @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
-           @ship_1 = Ship.new(params["pirate"]["ships"][0]["name"],params["pirate"]["ships"][0]["type"], params["pirate"]["ships"][0]["booty"])
-           @ship_2 = Ship.new(params["pirate"]["ships"][1]["name"], params["pirate"]["ships"][1]["type"], params["pirate"]["ships"][1]["booty"])
-           erb :show
-         end
+ 
+ module FormsLab
+   class App < Sinatra::Base
+  
+      # code other routes/actions here
+  
+     get '/' do
+       erb :root
+     end
+ 
+     get '/new' do
+       erb :'pirates/new'
+     end
+ 
+     post '/pirates' do
+       @pirate = Pirate.new(params[:pirate])
+       params[:pirate][:ships].each do |ship_details|
+         Ship.new(ship_details)
+       end
+ 
+       @ships = Ship.all
+ 
+       erb :'pirates/show'
+     end
+ 
+    end
   end
-end
