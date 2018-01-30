46e870edcbebcccdc010dbd7a95f4a466620e3ac
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 20:49:33 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,14 +2,14 @@ class Pirate
   attr_accessor :name, :weight, :height
  
      @@all = []
- 
+
      def initialize(arghs)
          @name = arghs[:name]
          @weight = arghs[:weight]
          @height = arghs[:height]
          @@all << self
      end
- 
+
      def self.all
          @@all
      end
